3e8b5294787e3ddb9f233d50024aad39b8732ce1
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-06-28 10:58:26 +0800

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -175,7 +175,7 @@ const Home: React.FC = () => {
               Let's discuss your requirements and create the IPL solution that best fit for your brand.
             </p>
             <Button variant="outline" size="lg" className="border-white text-white hover:bg-white hover:text-ishine-teal-500">
-              Free Consultation
+             Fill out the form to enjoy a Free Consultation Free Consultation
             </Button>
           </motion.div>
         </div>
