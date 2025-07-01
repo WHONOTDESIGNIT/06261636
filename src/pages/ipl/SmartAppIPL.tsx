557b42cf3ff34e1127c90ea8123d665f77449aac
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-01 15:50:16 +0800

    Updated SmartAppIPL.tsx

--- src/pages/ipl/SmartAppIPL.tsx
+++ src/pages/ipl/SmartAppIPL.tsx
@@ -3,7 +3,6 @@ import {
   ChevronLeft,
   ChevronRight,
   Star,
-  ShoppingCart,
   Plus,
   Minus,
   Shield,
@@ -210,7 +209,6 @@ const SmartAppIPL: React.FC = () => {
                 </div>
               </div>
               <button className="w-full bg-[rgb(0,116,224)] hover:bg-[rgb(0,89,179)] text-white font-semibold py-4 rounded-lg flex items-center justify-center transition-colors">
-                <ShoppingCart className="w-5 h-5 mr-2" />
                 Get A Quote Now
               </button>
             </div>
