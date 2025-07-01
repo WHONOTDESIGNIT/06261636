fdd14f39bed742b7b5d3b89277005fb45e396efe
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-01 16:09:40 +0800

    Updated SmartAppIPL.tsx

--- src/pages/ipl/SmartAppIPL.tsx
+++ src/pages/ipl/SmartAppIPL.tsx
@@ -23,11 +23,8 @@ const SmartAppIPL: React.FC = () => {
     'https://i.postimg.cc/tCtVBTPz/app-ipl-three-set.webp',
     'https://i.postimg.cc/HLk5DHCr/app-ipl-side.webp',
     'https://i.postimg.cc/L8HgLR5x/app-ipl-lay.webp',
-    '/images/smart-app-ipl/app-usage.jpg',
-    '/images/smart-app-ipl/features.jpg',
-    '/images/smart-app-ipl/package.jpg',
-    '/images/smart-app-ipl/comparison.jpg',
-    '/images/smart-app-ipl/accessories.jpg',
+    'https://i.postimg.cc/hvWfjRNb/app-ipl-back.webp',
+
   ];
   const thumbnails = productImages;
 
