9e0027341f3a627d0a331c3605b1edbfd4ef3620
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-01 15:49:38 +0800

    Fix JSX syntax error in SmartAppIPL component

--- src/pages/ipl/SmartAppIPL.tsx
+++ src/pages/ipl/SmartAppIPL.tsx
@@ -210,7 +210,7 @@ const SmartAppIPL: React.FC = () => {
                 </div>
               </div>
               <button className="w-full bg-[rgb(0,116,224)] hover:bg-[rgb(0,89,179)] text-white font-semibold py-4 rounded-lg flex items-center justify-center transition-colors">
-                <className="w-5 h-5 mr-2" />
+                <ShoppingCart className="w-5 h-5 mr-2" />
                 Get A Quote Now
               </button>
             </div>
