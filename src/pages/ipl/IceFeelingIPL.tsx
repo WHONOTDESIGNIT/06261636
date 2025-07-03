20838c9563a425a8b88b195cd08375d9164bd7d9
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-03 14:37:37 +0800

    Updated IceFeelingIPL.tsx

--- src/pages/ipl/IceFeelingIPL.tsx
+++ src/pages/ipl/IceFeelingIPL.tsx
@@ -217,7 +217,7 @@ const IceFeelingIPL: React.FC = () => {
                 </div>
               </div>
               <button className="w-full bg-[rgb(0,116,224)] hover:bg-[rgb(0,89,179)] text-white font-semibold py-4 rounded-lg transition-colors flex items-center justify-center">
-                <className="w-5 h-5 mr-2" />
+              
                GET A QUOTE NOW
               </button>
             </div>
