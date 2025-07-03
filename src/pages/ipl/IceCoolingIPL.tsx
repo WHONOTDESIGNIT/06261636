9fc7295a331209f40442852a3f1204416a8c8706
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-03 14:50:25 +0800

    Updated IceCoolingIPL.tsx

--- src/pages/ipl/IceCoolingIPL.tsx
+++ src/pages/ipl/IceCoolingIPL.tsx
@@ -44,7 +44,7 @@ const IceCoolingIPL: React.FC = () => {
         <nav className="text-sm text-gray-500 mb-4 mt-4 uppercase">
           <a href="/" className="hover:text-blue-600">Home</a>
           <span className="mx-2">/</span>
-          <a href="/products" className="hover:text-blue-600">IPL Hair Removal</a>
+          <a href="/ipl-hair-removal" className="hover:text-blue-600">IPL Hair Removal</a>
           <span className="mx-2">/</span>
           <span className="text-gray-800">Ice Cooling IPL Hair Removal Device</span>
         </nav>
