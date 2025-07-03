da7089e5ee9e84da225b907d69d6ebcd6093dd6c
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-03 14:28:53 +0800

    Updated IceFeelingIPL.tsx

--- src/pages/ipl/IceFeelingIPL.tsx
+++ src/pages/ipl/IceFeelingIPL.tsx
@@ -22,7 +22,7 @@ const IceFeelingIPL: React.FC = () => {
   /* ========= 1. 资源配置 ========= */
   const productImages = [
     'https://i.postimg.cc/wMrKRZrf/sapphire-IPL.webp',
-    '/images/ice-feeling-ipl/interface.jpg',
+    'https://i.postimg.cc/76BdjjqH/sapphire-ipl-black.webp',
     '/images/ice-feeling-ipl/side.jpg',
     '/images/ice-feeling-ipl/usage.jpg',
     '/images/ice-feeling-ipl/features.jpg',
