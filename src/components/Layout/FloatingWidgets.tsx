f2572657213a39ce20834fe0d6844245b53f3670
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-04 18:39:44 +0800

    Updated FloatingWidgets.tsx

--- src/components/Layout/FloatingWidgets.tsx
+++ src/components/Layout/FloatingWidgets.tsx
@@ -20,7 +20,7 @@ const FloatingWidgets: React.FC = () => {
   };
 
   const openWhatsApp = () => {
-    window.open('https://wa.me/1234567890', '_blank');
+    window.open('https://wa.me/8615012891148', '_blank');
   };
 
   return (
