5d2fc9e579eb0ed1f91c786244072ab787f0766c
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-14 15:39:50 +0800

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -361,7 +361,7 @@ export default function HomePage() {
                   <img
                     src={unlimitedOptions[optionIdx].content.img}
                     alt={unlimitedOptions[optionIdx].label}
-                    className="w-[650px] h-[650px] object-contain rounded-xl shadow"
+                    className="w-[600px] h-[480px] object-contain rounded-xl shadow"
                   />
                 )}
                 <div className="flex-1 text-left">
