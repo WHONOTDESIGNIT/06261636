2b75e2c1a0ecba9e0633c906e80a7115f8a52387
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-18 15:59:26 +0800

    Updated useTranslation.ts

--- src/hooks/useTranslation.ts
+++ src/hooks/useTranslation.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useTranslation as useI18nextTranslation } from 'react-i18next';
 
 interface TranslationData {
