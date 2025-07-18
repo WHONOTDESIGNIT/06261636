8a8bd95c515ceb6aa4245677ec24c1b8f3585c77
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-18 16:00:54 +0800

    Updated i18next-http-backend.d.ts

--- src/types/i18next-http-backend.d.ts
+++ src/types/i18next-http-backend.d.ts
@@ -1,11 +1,4 @@
 declare module 'i18next-http-backend' {
-  import { BackendOptions, RequestCallback } from 'i18next';
-
-  class HttpBackend {
-    constructor(options?: BackendOptions);
-    read(language: string, namespace: string, callback: RequestCallback): void;
-    // Add other methods if needed
-  }
-
-  export default HttpBackend;
+  const content: any;
+  export default content;
 } 
