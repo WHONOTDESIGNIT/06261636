f303249180f6365d6dc5c2eac7f80e20c3d97042
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-23 14:33:29 +0800

    Deleted publish-post.js

--- netlify/functions/publish-post.js
+++
@@ -1,9 +0,0 @@
-import { getStore } from '@netlify/blobs';
-
-export async function handler(event, context) {
-  const { title, content } = JSON.parse(event.body);
-  const id = Date.now().toString();
-  const store = getStore({ name: 'BlogPosts', consistency: 'strong' });
-  await store.set(id, JSON.stringify({ title, content }));
-  return { statusCode: 200, body: '发布成功' };
-} 
