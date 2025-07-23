18b88e1f44401c663da2c848edf80a9eba26c65d
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-23 14:33:22 +0800

    Deleted delete-post.js

--- netlify/functions/delete-post.js
+++
@@ -1,8 +0,0 @@
-import { getStore } from '@netlify/blobs';
-
-export async function handler(event, context) {
-  const { id } = JSON.parse(event.body);
-  const store = getStore({ name: 'BlogPosts', consistency: 'strong' });
-  await store.delete(id);
-  return { statusCode: 200, body: '删除成功' };
-} 
