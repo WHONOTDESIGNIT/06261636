f398d77d0560aaa802dba8e3a617870c4dc71c7d
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-23 14:33:23 +0800

    Deleted edit-post.js

--- netlify/functions/edit-post.js
+++
@@ -1,8 +0,0 @@
-import { getStore } from '@netlify/blobs';
-
-export async function handler(event, context) {
-  const { id, title, content } = JSON.parse(event.body);
-  const store = getStore({ name: 'BlogPosts', consistency: 'strong' });
-  await store.set(id, JSON.stringify({ title, content }));
-  return { statusCode: 200, body: '修改成功' };
-} 
