94b6538da1064eb227974cf8f10104b154824a8f
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-23 12:16:57 +0800

    Updated publish-post.js

--- netlify/functions/publish-post.js
+++ netlify/functions/publish-post.js
@@ -1,9 +1,9 @@
-const { getStore } = require('@netlify/blobs');
+import { getStore } from '@netlify/blobs';
 
-exports.handler = async (event) => {
+export async function handler(event, context) {
   const { title, content } = JSON.parse(event.body);
   const id = Date.now().toString();
   const store = getStore({ name: 'BlogPosts', consistency: 'strong' });
   await store.set(id, JSON.stringify({ title, content }));
   return { statusCode: 200, body: '发布成功' };
-}; 
+} 
