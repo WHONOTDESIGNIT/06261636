3d3e07b76b116dea1cab4d4adcf5bb76890f1ff8
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-07-31 14:59:47 +0800

    Fix sitemap script ES module syntax

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // 网站基础信息
 const SITE_URL = 'https://iplmanufacturer.com';
