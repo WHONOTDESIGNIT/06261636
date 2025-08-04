345ac3772aa44028e1fa739879f4f75e151140cc
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-08-04 17:31:53 +0800

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 const nextIntlPlugin = require('next-intl/plugin');
 
-module.exports = nextIntlPlugin()({
-  i18n: {
-    locales: ['en', 'ar', 'de', 'es', 'he', 'nl', 'pl', 'pt'],  // 从您的 translations 目录获取
-    defaultLocale: 'en'
-  }
-});
+module.exports = nextIntlPlugin();
