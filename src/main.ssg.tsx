97c76f70fe50d89b229f8f49fd3561bcd4f60682
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-08-04 16:38:29 +0800

    Updated main.ssg.tsx

--- src/main.ssg.tsx
+++ src/main.ssg.tsx
@@ -2,8 +2,14 @@ import { ViteSSG } from 'vite-ssg'
 import App from './App'
 import './index.css'
 
+const routes = []
+
 // `export const createApp` is required for vite-ssg
 export const createApp = ViteSSG(
   // The root component
-  App
+  App,
+  { routes },
+  ({ app, router, routes, isClient, initialState }) => {
+    // configure your app
+  }
 )
